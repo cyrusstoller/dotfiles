f87f8a561494ca68713e401cdffddaf2a1150c5d
Cyrus Stoller <cyrus.stoller@gmail.com>
2020-01-13 17:29:48 -0800

    Minor changes to homebrew install

--- install-brew.sh
+++ install-brew.sh
@@ -8,6 +8,7 @@ if [ $? != 0 ]; then
 else
     echo "Updating homebrew ..."
     brew update
+    brew upgrade
 fi
 
 BASEDIR="$( cd "$(dirname "$0")" ; pwd -P )"
@@ -22,7 +23,7 @@ echo
 homebrew_package_list="$BASEDIR/homebrew/brewlist"
 while read brew_pkg
 do
-    if brew ls --verions $brew_pkg > /dev/null; then
+    if brew ls --versions $brew_pkg 2> /dev/null; then
         echo "$brew_pkg is already installed"
     else
         echo $brew_pkg
@@ -31,7 +32,15 @@ do
 done < $homebrew_package_list
 
 # Start services like postgresql on boot
+echo
+echo "Ensuring that all brew services are started"
 brew services start --all
+
+if brew ls --versions heroku 2> /dev/null; then
+    echo "Heroku CLI already installed"
+else
+    brew tap heroku/brew && brew install heroku
+fi
 
 echo
 echo "Ensuring that the necessary brew cask packages are installed"
@@ -41,7 +50,7 @@ echo
 homebrew_cask_list="$BASEDIR/homebrew/casklist"
 while read cask_pkg
 do
-    if brew cask ls --versions $cask_pkg > /dev/null; then
+    if brew cask ls --versions $cask_pkg 2> /dev/null; then
         echo "$cask_pkg is already installed"
     else
         echo "$cask_pkg"
